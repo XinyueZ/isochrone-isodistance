14a32c1496b4ad3581aa5ffd9994d0082ca717fa
XinyueZ <xinyue.zhao@immonet.de>
2018-08-09 10:09:07 +0200

    Remove sample codes from MainActivity

--- app/src/main/java/com/demo/mvp/findlocation/MainActivity.kt
+++ app/src/main/java/com/demo/mvp/findlocation/MainActivity.kt
@@ -5,18 +5,9 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import com.demo.mvp.R
-import com.demo.mvp.algorithm.getIsochrone
-import com.demo.mvp.algorithm.pretty
 import com.demo.mvp.databinding.ActivityMainBinding
 import com.demo.mvp.inject
-import com.demo.mvp.net.CoroutinesContextProvider
-import com.demo.mvp.provideGoogleApiKey
-import com.google.android.gms.maps.model.LatLng
-import kotlinx.coroutines.experimental.channels.ReceiveChannel
-import kotlinx.coroutines.experimental.channels.consumeEach
-import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,24 +29,5 @@ class MainActivity : AppCompatActivity() {
             REQUEST_CHECK_SETTINGS -> binding?.view?.getCurrentLocation()
             else -> super.onActivityResult(requestCode, resultCode, data)
         }
-    }
-
-    private var channel: ReceiveChannel<Array<LatLng>>? = null
-
-    override fun onResume() {
-        super.onResume()
-        launch(CoroutinesContextProvider.main) {
-            getIsochrone(provideGoogleApiKey(applicationContext), "Große Elbstraße 39", 120)?.let {
-                channel = it
-                channel?.consumeEach {
-                    Log.d("algorithm", "rad1: ${it.pretty()}")
-                }
-            }
-        }
-    }
-
-    override fun onPause() {
-        channel?.cancel()
-        super.onPause()
     }
 }
