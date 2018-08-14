5f912df6378bcd20fdb15cc0a48f9e4be86fa3e3
XinyueZ <xinyue.zhao@immonet.de>
2018-08-14 11:54:02 +0200

    Add query.kt for queryMatrix and queryGeocode

--- library/src/main/java/isochrone/isodistance/android/algorithm/isochrone.kt
+++ library/src/main/java/isochrone/isodistance/android/algorithm/isochrone.kt
@@ -5,12 +5,9 @@ import com.google.android.gms.maps.model.LatLng
 import isochrone.isodistance.android.domain.geocode.Geocode
 import isochrone.isodistance.android.domain.matrix.Matrix
 import isochrone.isodistance.android.net.Result
-import isochrone.isodistance.android.net.provideApi
 import isochrone.isodistance.android.utils.CoroutinesContextProvider
 import kotlinx.coroutines.experimental.channels.ProducerScope
 import kotlinx.coroutines.experimental.channels.produce
-import retrofit2.Response
-import java.io.IOException
 import java.lang.Math.PI
 import java.lang.Math.asin
 import java.lang.Math.atan2
@@ -173,41 +170,6 @@ private fun Matrix.calculateAddressesDurations(): Pair<Array<String>, Array<Doub
     } ?: run { return null }
 }
 
-private suspend fun queryMatrix(
-    travelMode: TravelMode,
-    origin: LatLng,
-    destinations: Array<String>,
-    key: String
-): Result<Matrix> {
-    val destinationsString = destinations.joinToString("|")
-    val originString = "${origin.latitude}, ${origin.longitude}"
-    val response =
-        provideApi().getMatrix(travelMode.value, originString, destinationsString, key).await()
-    return response.getResult {
-        Result.Error(
-            IOException("Error query matrix: $origin ====> ${destinations.pretty()}")
-        )
-    }
-}
-
-private suspend fun TravelMode.queryMatrix(
-    origin: LatLng,
-    destinations: Array<LatLng>,
-    key: String
-): Result<Matrix> {
-    val destinationsStringList = destinations.map { "${it.latitude}, ${it.longitude}" }
-    return queryMatrix(this, origin, destinationsStringList.toTypedArray(), key)
-}
-
-private suspend fun queryGeocodeAddress(address: String, key: String): Result<Geocode> {
-    val response = provideApi().getGeocode(address, key).await()
-    return response.getResult {
-        Result.Error(
-            IOException("Error query geocode for address: $address")
-        )
-    }
-}
-
 private fun selectDestination(origin: LatLng, angle: Double, radius: Double): LatLng {
     val bearing = toRadians(angle)
     val lat1 = toRadians((origin.latitude))
@@ -245,16 +207,6 @@ private fun sortPoints(origin: LatLng, iso: Array<LatLng>) =
     }.zip(iso).sortedBy { it.first }.map { it.second }.toTypedArray()
 
 fun <T> Array<T>.pretty(): String = java.util.Arrays.toString(this)
-
-private inline fun <E : Any> Response<E>.getResult(onError: () -> Result.Error): Result<E> {
-    if (isSuccessful) {
-        val body = body()
-        if (body != null) {
-            return Result.Success(body)
-        }
-    }
-    return onError.invoke()
-}
 
 private fun Geocode.toLatLng(): LatLng? = results?.let { results[0] }?.run {
     LatLng(geometry.location.lat, geometry.location.lng)
---
+++ library/src/main/java/isochrone/isodistance/android/algorithm/query.kt
@@ -0,0 +1,52 @@
+package isochrone.isodistance.android.algorithm
+
+import com.google.android.gms.maps.model.LatLng
+import isochrone.isodistance.android.domain.geocode.Geocode
+import isochrone.isodistance.android.domain.matrix.Matrix
+import isochrone.isodistance.android.net.Result
+import isochrone.isodistance.android.net.provideApi
+import retrofit2.Response
+import java.io.IOException
+
+internal inline fun <E : Any> Response<E>.getResult(onError: () -> Result.Error): Result<E> {
+    if (isSuccessful) {
+        val body = body()
+        if (body != null) {
+            return Result.Success(body)
+        }
+    }
+    return onError.invoke()
+}
+
+internal suspend fun queryGeocodeAddress(address: String, key: String): Result<Geocode> {
+    val response = provideApi().getGeocode(address, key).await()
+    return response.getResult {
+        Result.Error(IOException("Error query geocode for address: $address"))
+    }
+}
+
+internal suspend fun TravelMode.queryMatrix(
+        origin: LatLng,
+        destinations: Array<LatLng>,
+        key: String
+): Result<Matrix> {
+    val destinationsStringList = destinations.map { "${it.latitude}, ${it.longitude}" }
+    return queryMatrix(this, origin, destinationsStringList.toTypedArray(), key)
+}
+
+internal suspend fun queryMatrix(
+        travelMode: TravelMode,
+        origin: LatLng,
+        destinations: Array<String>,
+        key: String
+): Result<Matrix> {
+    val destinationsString = destinations.joinToString("|")
+    val originString = "${origin.latitude}, ${origin.longitude}"
+    val response =
+            provideApi().getMatrix(travelMode.value, originString, destinationsString, key).await()
+    return response.getResult {
+        Result.Error(
+                IOException("Error query matrix: $origin ====> ${destinations.pretty()}")
+        )
+    }
+}
