b5baa3f9de6f7226a95b7bd17b883ba2b8194839
XinyueZ <xinyue.zhao@immonet.de>
2018-08-14 16:27:55 +0200

    Adjust for klint

--- library/src/main/java/isochrone/isodistance/android/algorithm/config.kt
+++ library/src/main/java/isochrone/isodistance/android/algorithm/config.kt
@@ -7,7 +7,6 @@ internal const val DEFAULT_NUMBER_OF_ANGLES = 12
 // For isochrone
 internal const val EARTH_RADIUS: Double = 3963.1676
 internal const val TOLERANCE = 0.01
-
 internal const val SORT_RESULT = false
 
 // For isodistance
